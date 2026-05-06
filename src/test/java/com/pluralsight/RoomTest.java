710e9a03a9371c9c016e7cc76b199dbaea9a68d7
christian-nieves <thechristiannieves@gmail.com>
2026-05-06 09:44:03 -0400

    Check out test done

--- src/test/java/com/pluralsight/RoomTest.java
+++ src/test/java/com/pluralsight/RoomTest.java
@@ -1,4 +1,37 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RoomTest {
-  
+
+    @Test
+    public void checkIn_RoomStatus_RoomIsOccupiedAndDirty() {
+        // Arrange
+        Room room123 = new Room(2, 229);
+
+        // Act
+        room123.checkIn();
+
+        // Assert
+        assertTrue(room123.isOccupied());
+        assertTrue(room123.isDirty());
+    }
+
+    @Test
+    public void checkout() {
+        // Arrange
+        Room room456 = new Room(1, 100);
+
+        // Act
+        room456.checkout();
+
+        // Assert
+        assertFalse(room456.isOccupied());
+    }
+
+    @Test
+    public void cleanRoom() {
+    }
 }
